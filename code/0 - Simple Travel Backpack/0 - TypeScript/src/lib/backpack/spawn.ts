2698a8cda831bcfb0c564c897a61114c37056def
Endermen76428 <endermenender18@gmail.com>
2026-09-12 18:36:13 -0300

    finished lib

--- code/0 - Simple Travel Backpack/0 - TypeScript/src/lib/backpack/spawn.ts
+++ code/0 - Simple Travel Backpack/0 - TypeScript/src/lib/backpack/spawn.ts
@@ -1,9 +1,11 @@
-import { world, Entity, Player } from "@minecraft/server"
-import { backpackSizeEvent } from "../../functions/place"
+import { Entity, Player } from "@minecraft/server"
+import { getBackpackTier } from "./tier"
 
 export function spawnBackpack(player: Player, itemId: string): Entity {
-  const inventorySize = backpackSizeEvent[itemId] ?? 0
-  const entity = player.dimension.spawnEntity("travel_backpack:backpack", player.location, {spawnEvent: `travel_backpack:inventory${inventorySize}`})
-  entity.nameTag = `ui.travel_backpack:backpack.size.${inventorySize}`
+  let inventoryTier = getBackpackTier(itemId)
+  const entity = player.dimension.spawnEntity("travel_backpack:backpack", player.location, {spawnEvent: `travel_backpack:inventory${inventoryTier}`})
+  entity.nameTag = `ui.travel_backpack:backpack.size.${inventoryTier}`
+  entity.addTag(`travel_backpack:${player.id}`)
+
   return entity
 }
